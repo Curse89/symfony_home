fb8d369f279bff927648a3c8af68454c335935a5
Сергей Кривич <krivich@retailcrm.ru>
2022-05-18 21:57:45 +0300

    Vue 2 PRE-SET

---
+++ assetsv2/src/components/Learn.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <div>{{ msg }}</div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class Learn extends Vue {
+  @Prop({ default: "default value" })
+  private msg: string;
+}
+</script>
---
+++ assetsv2/src/views/LearnView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <div class="home">VIEW TEMPLATE</div>
+    <Learn />
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import Learn from "@/components/Learn.vue"; // @ is an alias to /src
+
+@Component({
+  components: {
+    Learn,
+  },
+})
+export default class HomeView extends Vue {}
+</script>
